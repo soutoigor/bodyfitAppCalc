9e68b93fa8ba55adcb902307759a3951a173437e
Igor Souto <igor_souto@outlook.com>
2018-07-13 14:47:07 -0300

    cancelado medidas, futura att

--- root/www/js/medidas.js
+++ root/www/js/medidas.js
@@ -1,16 +1,47 @@
+
 (function (){
-    
-    document.querySelector('#btn-show-medidas').addEventListener('click', function(){
-        
-       
-        $(this).parent().find('div.accordion').slideToggle("slow");
-        
-           // document.querySelector('#area-medidas').className = "show-insere-medidas";
-         //   document.querySelector('#area-medidas').style.transition = "all 2s linear"; 
-        
-            
-    })
-    
-    
-    
-})()
+  var $nome = document.getElementById('nome');
+  var $altura = document.getElementById('altura');
+  var $peso = document.getElementById('peso');
+  var $abdomen = document.getElementById('abdomen');
+  var $quadril = document.getElementById('quadril');
+  var $bracos = document.getElementById('bracos');
+  var $coxas = document.getElementById('coxas');
+  var $panturrilha = document.getElementById('panturrilha');
+
+  var $consultar = document.getElementById('consultar');
+  var $form = document.getElementById('frmMedidas');
+
+  $consultar.addEventListener('click', getMedidas());
+
+  $form.addEventListener('submit', setMedidas());
+
+
+
+
+  function setMedidas(){
+    return function(){
+    window.localStorage.setItem("nome", $nome.value);
+    window.localStorage.setItem("altura", $altura.value);
+    window.localStorage.setItem("peso", $peso.value);
+    window.localStorage.setItem("abdomen", $abdomen.value);
+    window.localStorage.setItem("quadril", $quadril.value);
+    window.localStorage.setItem("bracos", $bracos.value);
+    window.localStorage.setItem("coxas", $coxas.value);
+    window.localStorage.setItem("panturrilha", $panturrilha.value);
+
+}
+}
+
+  function getMedidas(){
+    return function (){
+
+    }
+  }
+
+
+
+
+
+
+    })()
